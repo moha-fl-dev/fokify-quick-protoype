622ec1bee13f11edba7654a63a72bfd0fe6dddeb
moha-fl-dev <moha_ali@live.nl>
2022-06-13 17:15:07 +0200

    forkify-quick-build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState, useRef } from 'react';
+
 function App() {
+
+  const [search, setSearch] = useState('pizza');
+  const [recipes, setRecipes] = useState([]);
+
+  const searchRef = useRef();
+
+  useEffect(() => {
+
+    (async () =>{
+      const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${search}`);
+      const data = await response.json();
+
+      const {recipes} = data;
+      
+
+      setRecipes(recipes)
+
+    })()
+
+    }, [search])
+
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    setSearch(searchRef.current.value);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Forkify Recipes</h1>
       </header>
+
+      <div className='search'>
+        <form>
+            <input type='text' placeholder='Search' ref={searchRef}/>
+            <button type='submit' onClick={searchHandler}>Search</button>
+        </form>
+      </div>
+      <div className='recipe-container'>
+          {
+          recipes.length > 0 ? recipes.map((recipe, index) =>{
+            const {title, image_url, publisher, source_url} = recipe;
+            return (
+              <div key={index} className='recipe'>
+                
+                <img src={image_url} alt={title}/>
+                <div><p>{title}</p></div>
+                <p>Publisher: {publisher}</p>
+                <a href={source_url}>Source</a>
+              </div>
+            )
+          }) : <p>Loading....</p>
+          }
+      </div>
+     
+
     </div>
   );
 }
